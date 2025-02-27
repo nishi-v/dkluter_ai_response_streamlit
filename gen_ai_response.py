a3f712676d0bc630951d89037353e6cd749ecff1
nishi-v <nishitav@futuresoftindia.com>
2025-02-27 17:14:13 +0530

    modified py script

--- gen_ai_response.py
+++ gen_ai_response.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from PIL import Image
 import json
 import re
+from google.genai import types
 
 # Set current working directory
 dir = Path(os.getcwd())
@@ -24,15 +25,18 @@ GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
 image = Image.open(img_file)
 
 prompt_text = """
-Analyze the given image and generate a concise, accurate title. If there is a brand name or title, identify it. The description should focus only on the foreground object(s) and be no more than 80 words, providing relevant details about their characteristics, actions, or context while ignoring the background. The tags should be structured hierarchically, first provide relevant tags starting from broad categories, moving on to more specific attributes. Avoid color-related visual tags. 
+Analyze the given image and generate a concise, accurate title. If there is a brand name or title, identify it. 
 
-View this as an extension to OCR when text is present - extract all relevant details and supply them in the appropriate fields. For example, for an image of a book - identify the title, author, ISBN or other relevant details that are present and visible. 
+The description should focus only on the foreground object(s) and be no more than 80 words, providing relevant details about their characteristics, actions, or context while ignoring the background. 
 
-Return the response strictly in the following JSON format, without any additional text, explanation, or preamble. Tags should be structured from most general to most specific, with abstract tags being placed at the end. 
+The tags should be structured hierarchically, first provide relevant tags starting from broad categories, moving on to more specific attributes. Avoid color-related visual tags. 
+
+View this as an extension to OCR when text is present - extract all relevant details and supply them in the appropriate fields. 
+
+Return the response strictly in the following JSON format, without any additional text, explanation, or preamble.
 
 One example format:
 {
-  "Message": "success message/Error message",
   "Data": {
     "title": "Modern Green Sofa with Elegant Vertical Stitching",
     "description": "Add a touch of sophistication and comfort to your living space with this stylish green sofa. Featuring a contemporary design, the sofa boasts soft, velvety upholstery with elegant vertical stitching on the backrest. The deep green tone complements various color schemes, making it perfect for modern or retro-inspired interiors. Supported by sturdy wooden legs, this sofa is as durable as it is visually appealing, offering both style and comfort for any room.",
@@ -42,15 +46,18 @@ One example format:
       "Sofa",
       "Design"  
     ]
-  },
-  "Status": "ok/error"
+  }
 }
 """
 
 client = genai.Client(api_key=GEMINI_API_KEY)
 response = client.models.generate_content(
     model="gemini-2.0-flash",
-    contents=[image, prompt_text])
+    contents=[image, prompt_text],
+    config=types.GenerateContentConfig(
+        temperature=0.000001
+    ))
+
 # print("API Response:", response.text)
 
 if response.text:
