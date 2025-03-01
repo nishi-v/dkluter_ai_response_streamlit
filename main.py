329d3837b0cb1c724bf9bfce30da515272bfe717
Puranjay <purnaj@futuresoftindia.com>
2025-03-01 17:42:35 +0530

    Update prompt text and add token counts

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def get_image(img_file: str) -> Image.Image:
         print(f"Error fetching {img_file}: {e}")
         return None # type: ignore
 
-def gen_batch_response(api: str, img: Image.Image, types: list) -> tuple[dict, float]:
+def gen_batch_response(api: str, img: Image.Image, types: list, prompt_text: str, count_tokens: bool = False) -> tuple[dict, float]:
     # Generate AI Response
     client = genai.Client(api_key = api)
     model_id = "gemini-2.0-flash"
@@ -27,53 +27,15 @@ def gen_batch_response(api: str, img: Image.Image, types: list) -> tuple[dict, f
         google_search = GoogleSearch(),
     )
 
-    prompt_text = """
-        Analyze the given image and the types given with it, and generate a concise, accurate title. If there is a brand name or title, identify it with a max length of 100 characters.
-    
-        The description should focus only on the foreground object(s) and be no more than 80 words, providing relevant details about their characteristics, actions, or context while ignoring the background. If you are able to identify a known product in the image, please provide its details in the description rather than a simple captioning of what it visible. E.g., for a known book by an author, mention when the book was released, the publisher, the genre, and a short summary of what the book is about rather than just captioning the text and image on the front cover of the book.    
-        
-        The tags should be structured hierarchically, first provide relevant tags starting from broad categories, moving on to more specific attributes. Avoid color-related visual tags. Max limit to tags is 50 characters.
-        
-        ALWAYS generate relevant field name, type and value based on the objects and types provided. Field names should ALWAYS be in list format.
-    
-        Field should ALWAYS be present and NON EMPTY. If a field value cannot be determined, provide the best possible field. If possible add LOCATION type Field if found.
-
-        Each field **must** have:
-        - A meaningful 'field_name' (max 50 characters).
-        - A valid 'field_type' (TEXT, NUMBER, DATE, LOCATION).
-        - A **non-empty 'field_value'** (max 500 characters).
-
-        Ensure that **no field_name, field_type, or field_value is missing**.
-            
-        View this as a smarter OCR when text is present - extract all relevant details and supply them in the appropriate fields. Use the search tool to identify relevant attributes. In case of a known product, identify model number, SKU, etc. E.g., for an image of a book - make sure to always search and fetch the title, author, publisher, ISBN number. Then, provide other relevant details from the web that will be useful to know for the user. Provide fields with values as objects within "tags" as a key-value pair.
-        Return the response strictly in the following JSON format, without any additional text, explanation, or preamble.
-        Example format:
-        {
-        "Data": {
-            "title": "To Kill a Mockingbird by Harper Lee",
-            "description": "A gripping, heart-wrenching, and wholly remarkable tale of coming-of-age in a South poisoned by virulent prejudice. It views a world of great beauty and savage inequities through the eyes of a young girl, as her father—a crusading local lawyer—risks everything to defend a black man unjustly accused of a terrible crime.",
-            "tags": [
-            "Book",
-            "Classic",
-            "Literature",
-            "Novel",
-            "Justice"
-            ],
-            "fields": [
-                {
-                    "field_name": "ISBN",
-                    "field_type": "number",
-                    "field_value": ["9780061120084"]
-                },
-                {
-                    "field_name": "Genre",
-                    "field_type": "text",
-                    "field_value": ["Fiction"]
-                }
-                ]}
-        }
-        """ 
+    prompt_text = prompt_text
     prompt_text += f'\nThese are the tag categories to which the object belongs, which you can use as an additional reference to narrow down your search domain: "types": {types}'
+    
+    if count_tokens:
+        token_count = client.models.count_tokens(
+            model='gemini-2.0-flash',
+            contents=prompt_text,
+        )
+    print(f"Prompt tokens: {token_count}")
     # print(prompt_text)
 
     start_time = time.time()
@@ -126,6 +88,50 @@ def main():
     df = pd.read_csv(csv_file)  
     # print(df)
 
+    prompt_text = """
+        Analyze the given image and types to generate: title, description, tags, fields. Title should be concise, accurate. If there is a brand name or title, identify it. Max length 100 characters.
+    
+        Description should focus only on the foreground object(s) and be no more than 80 words, providing relevant details about their characteristics, actions, or context while ignoring the background. If you are able to identify a known product in the image, provide its details rather than a simple captioning of what is visible. E.g., for a known book by an author, mention when the book was released, publisher, genre, and short summary of what the book is about rather than just captioning the text and image on the front cover of the book. Avoid using demonstrative pronouns ('this is', 'these are').  
+        
+        Tags should be structured hierarchically. First provide relevant tags starting from broad categories, moving on to  specific attributes. Tag max length is 50 characters. ALWAYS generate at least 3 tags.
+        
+        Avoid color-related visual tags and fields. 
+        
+        ALWAYS generate relevant field name, type, value based on the objects and types provided. Field names should ALWAYS be in list format.
+    
+        Field should ALWAYS be present and NON EMPTY. If field value can't be determined, provide best possible field. Field type can be: TEXT, NUMBER, DATE, LOCATION. Categorize alphanumeric values ('12 AB'), or numbers with units ('15 mins') as TEXT. Numeric values ('123 45') should have whitespace removed and be categorized as NUMBER. If possible, add LOCATION type Field.
+
+        Each field must have: Meaningful 'field_name' (max 50 characters), Valid 'field_type', Non-empty 'field_value' (max 500 characters)
+            
+        Use search tool to identify relevant attributes. In case of a known product, identify model number, SKU, etc. E.g., for image of a book - make sure to always search and fetch the title, author, publisher, ISBN number. Then, provide other relevant details from the web that will be useful to know for the user. 
+        
+        Return the response strictly in following JSON format, without additional text, explanation, or preamble:
+        {
+        "Data": {
+            "title": "To Kill a Mockingbird by Harper Lee",
+            "description": "A gripping, heart-wrenching, and wholly remarkable tale of coming-of-age in a South poisoned by virulent prejudice. It views a world of great beauty and savage inequities through the eyes of a young girl, as her father—a crusading local lawyer—risks everything to defend a black man unjustly accused of a terrible crime.",
+            "tags": [
+            "Book",
+            "Classic",
+            "Literature",
+            "Novel",
+            "Justice"
+            ],
+            "fields": [
+                {
+                    "field_name": "ISBN",
+                    "field_type": "NUMBER",
+                    "field_value": ["9780061120084"]
+                },
+                {
+                    "field_name": "Genre",
+                    "field_type": "TEXT",
+                    "field_value": ["Fiction"]
+                }
+                ]}
+        }
+        """ 
+
     results = []
 
     for index, row in df.iterrows():
@@ -136,10 +142,10 @@ def main():
         types = row['Types'].split(', ') if isinstance(row['Types'], str) else []
 
         image = get_image(img_file)
-        print(f"Processing {index+1}: {img_name}") #type:ignore
+        print(f"Processing {index+1}: {img_name}") 
         try:
             # Generate Response
-            response_data, time_taken = gen_batch_response(GEMINI_API_KEY, image, types)
+            response_data, time_taken = gen_batch_response(GEMINI_API_KEY, image, types, prompt_text, count_tokens=True)
 
             print(f"Response: \n{response_data}")
             print(f"Time taken: {time_taken:.2f} seconds")
@@ -203,6 +209,11 @@ def main():
 
     # Format the Time column to have 2 decimal places
     results_df['Time'] = results_df['Time'].apply(lambda x: f"{x:.2f}")
+    
+    #Add to csv and print:
+    #Number of images processed
+    #Total time taken 
+    #Average time taken per prompt
     
     # Save the results to CSV, replacing the original file
     results_df.to_csv(csv_file, index=False)
